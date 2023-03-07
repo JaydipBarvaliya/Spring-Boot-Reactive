5a28bb57d6d1ece610e4465e1d7355b690ac29f5
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-03-06 20:16:10 -0500

    Package name & Structure Changes.

--- src/main/java/com/javabrains/reactive/SpringBootReactiveApplication.java
+++ src/main/java/com/javabrains/reactive/SpringBootReactiveApplication.java
@@ -1,4 +1,4 @@
-package com.javabrains.springbootreactive;
+package com.javabrains.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
