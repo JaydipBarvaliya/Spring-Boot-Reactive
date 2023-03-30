5013b19b3eebd74cfcff12d912e3edf8185ddcdf
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-03-30 03:19:25 -0400

    minor changes has been done.

--- src/main/java/com/javabrains/reactive/Exercise5.java
+++ src/main/java/com/javabrains/reactive/Exercise5.java
@@ -9,9 +9,9 @@ public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberFlux() and ReactiveSources.userMono()
+//         Use ReactiveSources.intNumberFlux() and ReactiveSources.userMono()
 
-        // Subscribe to a flux using the error and completion hooks
+//         Subscribe to a flux using the error and completion hooks
 //        ReactiveSources.intNumbersFlux().subscribe(event -> System.out.println(event),
 //                                                   err -> System.out.println(err.getMessage()),
 //                                                    () -> System.out.println("COMPLETING...")
@@ -21,8 +21,6 @@ public class Exercise5 {
 
         // Subscribe to a flux using an implementation of BaseSubscriber
         ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
-
-
 
         System.out.println("Press a key to end");
         System.in.read();
